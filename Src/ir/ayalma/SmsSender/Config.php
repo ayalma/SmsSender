ad7f4011f0bc5e24f02af2c39ce788a63e93c012
alimohammadi71 <alimohammadi350@gmail.com>
2016-04-21 20:21:05 +0430

    Initial commit

---
+++ Src/ir/ayalma/SmsSender/Config.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alimohammadi
+ * Date: 4/21/16
+ * Time: 7:32 PM
+ */
+namespace ir\ayalma\SmsSender;
+require "../../../../vendor/autoload.php";
+
+
+class Config
+{
+    private $_url;
+    private $_userName;
+    private $_password;
+    private $_number;
+
+    /**
+     * Config constructor.
+     * @param $_url : url of sms webservice.
+     * @param $_userName : username of user in webservice
+     * @param $_password : password of user.
+     * @param $_number   : phone number that sms sender give to you.
+     */
+    public function __construct($_url, $_userName, $_password, $_number)
+    {
+        $this->_url = $_url;
+        $this->_userName = $_userName;
+        $this->_password = $_password;
+        $this->_number = $_number;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->_url;
+    }
+
+    /**
+     * @param string $url
+     */
+    public function setUrl($url)
+    {
+        $this->_url = $url;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUserName()
+    {
+        return $this->_userName;
+    }
+
+    /**
+     * @param string $userName
+     */
+    public function setUserName($userName)
+    {
+        $this->_userName = $userName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->_password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->_password = $password;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNumber()
+    {
+        return $this->_number;
+    }
+
+    /**
+     * @param int $number
+     */
+    public function setNumber($number)
+    {
+        $this->_number = $number;
+    }
+    
+    
+
+
+}
---
+++ Src/ir/ayalma/SmsSender/SmsSender.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace ir\ayalma\SmsSender;
+
+require "../../../../vendor/autoload.php";
+
+use Exception;
+use SoapClient;
+
+/**
+ * Created by PhpStorm.
+ * User: alimohammadi
+ * Date: 4/18/16
+ * Time: 6:47 PM
+ */
+class SmsSender
+{
+    private static $_instance = null;
+
+    /**
+     * @var Config;
+     */
+    private $_config = null;
+    private $_client;
+
+    /**
+     * SmsSender constructor.
+     */
+    private function __construct()
+    {
+    }
+
+    /**
+     * @return SmsSender|null : return singleton instance of SmsSender.
+     */
+    public static function getInstance()
+    {
+        if (self::$_instance == null)
+            self::$_instance = new SmsSender();
+        return self::$_instance;
+    }
+
+    /**
+     *
+     * @param Config $config : configuration of SmsSender . call this method just one time in code.
+     */
+    public function init(Config $config)
+    {
+        $this->_config = $config;
+    }
+
+    /**
+     * connect to webservice.
+     * @throws Exception if config is null.
+     */
+    public function connect()
+    {
+        if ($this->_config == null)
+            throw new Exception('config is null , pleas call init first');
+
+        ini_set("soap.wsdl_cache_enabled", "0");
+        $this->_client = new SoapClient($this->_config->getUrl());
+    }
+
+    /**
+     * @param $mobile array : array of mobile number it must be smaller than 89.
+     * @param $message : message for sending.
+     * @param string $type : msg type related to @link {http://www.afe.ir/}
+     * @return mixed
+     * @throws Exception : throw exception if connect method not call first.
+     */
+    public function sendMessage($mobile, $message, $type = '')
+    {
+
+        if ($this->_client == null)
+            throw new Exception('client is null . pls call connect method first');
+
+        $method = 'SendMessage';
+
+        $param = array('Username' => $this->_config->getUserName(),
+            'Password' => $this->_config->getPassword(),
+            'Number' => "$this->_config->getNumber()",
+            'Mobile' => array("$mobile"),
+            'Message' => "$message",
+            'Type' => "$type");
+
+
+        $result = $this->_client->__SoapCall($method, array($param));
+
+
+        if ($type == 'object')
+            return get_object_vars($result);
+
+        $merge = $method . 'Result';
+
+        if ($result->$merge->string != '')
+            return $result->$merge->string;
+        else
+            return $result->$merge;
+    }
+
+    /**
+     * @param $massageId : id of message that wide get to you.
+     * @return mixed status of message.
+     * @throws Exception throw exception if connect method not call first.
+     */
+    public function getMessageStatus($massageId)
+    {
+
+        if ($this->_client == null)
+            throw new Exception('client is null . pls call connect method first');
+
+        $param = array('Username' => "$this->_config->getUserName()",
+            'Password' => "$this->_config->getPassword()",
+            'SmsID' => "$massageId");
+
+        $method = 'GetMessageStatus';
+
+        $result = $this->_client->__SoapCall($method, array($param));
+
+        $merge = $method . 'Result';
+        if ($result->$merge->string != '')
+            return $result->$merge->string;
+        else
+            return $result->$merge;
+
+    }
+
+
+}
